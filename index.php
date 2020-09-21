86f5abb96a58975a6a172f3b75245befcda20aa4
woow44410 <woow44410@gmail.com>
2020-09-21 16:32:46 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+    body {
+        font-family:微軟正黑體;
+    }
+</style>
+<title>蘇裕凱0903</title>
+</head>
+<body>
+<h2>蘇裕凱0903 Git練習記錄</h2>
+<hr>
+<?php include "menu.php"; ?>
+<hr>
+<?php include "footer.php"; ?>
+</body>
+</html>
